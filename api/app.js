e0a8b310674a314931a2f1feed8d5fe634864cfc
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-04 08:11:35 -0600

    debug image

--- api/app.js
+++ api/app.js
@@ -1,75 +1,60 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
 const app = express();
-const Post = require('./api/models/posts');
-const postsData = new Post();
-
-const slash = require('slash');
-
-const string = path.join('foo', 'bar');
-// Unix    => foo/bar
-// Windows => foo\\bar
-
-slash(string);
-// Unix    => foo/bar
-// Windows => foo/bar
-
-var multer = require('multer');
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads');
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
-  }
+const Post = require("./api/models/post");
+const postsData = new Post;
+const multer = require('multer');
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+     cb(null, './uploads')
+   },
+   filename: function (req, file, cb) {
+      cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
+   }
 })
-
-const getExt = (mimeType) => {
-  switch(mimeType) {
-    case 'image/png':
-      return '.png';
-    case 'image/jpeg':
-      return '.jpeg';
-  }
-};
-
-var upload = multer({ storage: storage });
-
-
-
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+const getExt = (mimetype) => {
+   switch (mimetype) {
+      case "image/png":
+         return ".png";
+      case "image/jpeg":
+         return ".jpeg";
+    }
+ }
+  
+const upload = multer({ storage: storage })
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   next();
+});
+
 app.use('/uploads', express.static('uploads'));
 
-app.get('/api/posts', (req, res) => {
-  res.status(200).send(postsData.get());
+app.get("/api/posts", (req, res) => {
+   res.status(200).send(postsData.get());
 });
 
-app.get('/api/posts/:post_id', (req, res) => {
-  const postId = req.params.post_id;
-  const foundPost = postsData.getIndividualBlog(postId);
-  if (foundPost) {
-    res.status(200).send(foundPost);
-  } else {
-    res.status(404).send('Not Found');
-  }
+app.get("/api/posts/:post_id", (req, res) => {
+   const postId = req.params.post_id;
+   const foundPost = postsData.getIndividualPost(postId);
+   if (foundPost) {
+      res.status(200).send(foundPost);
+   } else {
+      res.status(404).send("Not Found");
+   }
 });
 
-app.post('/api/posts', upload.single("post-image"), (req, res) => {
-  const newPost = {
-    'id': `${Date.now()}`,
-    'title': req.body.title,
-    'content': req.body.content,
-    'post_image': slash(req.file.path),
-    'added_date': `${Date.now()}`
-  }
-  postsData.add(newPost);
-  res.status(201).send(newPost);
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+   const newPost = {
+      "id": `${Date.now()}`,
+      "title": req.body.title,
+      "content": req.body.content,
+      "post_image": (req.file.path).replace(/\\/g, "/"),
+      "added_date": `${Date.now()}`
+   }
+   postsData.add(newPost);
+   res.status(201).send(newPost);
 });
 
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("listening on http://localhost:3000"));
